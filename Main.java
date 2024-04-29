abbfaeaff69e92be0a8b80502abd64dd70e016f6
cakeller2 <cakeller2@quinnipiac.edu>
2024-04-29 07:05:38 -0400

    fixing formatting

--- Main.java
+++ Main.java
@@ -4,8 +4,6 @@ import java.awt.event.ActionListener;
 import java.io.FileWriter;
 import java.io.File;  // Import the File class
 import java.io.IOException;  // Import the IOException class to handle errors
-
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,10 +14,7 @@ import git.tools.client.GitSubprocessClient;
 
 public class Main {
     public static void main (String[] args) {
-        
-
-           
-        
+    
         JFrame frame = new JFrame("Swing Demo");
         frame.setSize(800,600);
         frame.setLocationRelativeTo(null);
@@ -31,14 +26,14 @@ public class Main {
         frame.setContentPane(mainPanel);
 
         JTextField  box1;
-        box1= new JTextField("");  // box where user inputs his file path
+        box1= new JTextField("testing");  // box where user inputs his file path
         box1.setBounds(300,60, 250,50);
         mainPanel.add(box1);  
         mainPanel.setLayout(null);  
         mainPanel.setVisible(true); 
 
         JTextField  box2;
-        box2= new JTextField("");  // box where user inputs gitHub Username
+        box2= new JTextField("testing part 2");  // box where user inputs gitHub Username
         box2.setBounds(400,200, 250,50);
         mainPanel.add(box2);  
         mainPanel.setLayout(null);  
@@ -64,23 +59,13 @@ public class Main {
         mainPanel.add(repoNameInput);
         repoNameInput.setVisible(true);
 
-        
-
-      
-
-
-
-
         JButton ignoreButton = new JButton("2.Git Ignore and Readme");
         ignoreButton.setSize(200,50);
         ignoreButton.setLocation(50,350);
         ignoreButton.addActionListener(new ActionListener() {
             @Override
-
             public void actionPerformed(ActionEvent e) {
-                try{  
-
-
+                try {  
                     File readMe = new File("README.md");
                     if (readMe.createNewFile()){
                         System.out.println("README file Created" + readMe.getName());
@@ -88,45 +73,29 @@ public class Main {
                     } else {
                         System.out.println("File Already exists.");
                     }
-                }catch (IOException t){
+                } catch (IOException t){
                     System.out.println("An error occured.");
                     t.printStackTrace();
                 }
-                try{
-    
+
+                try {
                     File gitIgnore = new File(".gitignore");
-                if (gitIgnore.createNewFile()){
-                    System.out.println("Ignore file Created" + gitIgnore.getName());
-    
-                } else {
-                    System.out.println("File Already exists.");
-                }
-            }catch (IOException f){
+                    if (gitIgnore.createNewFile()){
+                        System.out.println("Ignore file Created" + gitIgnore.getName());
+                    } else {
+                        System.out.println("File Already exists.");
+                    }
+                } catch (IOException f){
                     System.out.println("An error occured.");
                     f.printStackTrace();
-            }
+                }
 
-            }
-               
-            
+            }          
+        });
 
-            
-        
-    });
-    
-    
-     
-    
-    
         mainPanel.add(ignoreButton);
-
         frame.setVisible(true);
             
-        
-       
-
-      
-
         JLabel gitUserName = new JLabel("Input your GitHub UserName");
         gitUserName.setSize(325,100);
         gitUserName.setLocation(70,45);
@@ -139,7 +108,6 @@ public class Main {
         mainPanel.add(output);
         output.setVisible(true);
 
-        
  
         JButton clickMeButton = new JButton("1.Turn Project to Repo");
         clickMeButton.setSize(200,50);
@@ -148,8 +116,6 @@ public class Main {
         JButton push = new JButton("3.Initial Push");
         push.setSize(200,50);
         push.setLocation(50,400);
-
-
         clickMeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -165,18 +131,9 @@ public class Main {
 
         });
 
-
-
-   
-
-    
-
         mainPanel.add(clickMeButton);mainPanel.add(push);
         frame.setVisible(true);
-
-
-
- }
+    }
 }
 
 
